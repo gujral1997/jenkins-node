cd122fb43694ae5a71e6478cde06f8b2c2c5a6df
Ansh Gujral <anshgujral1997@gmail.com>
2020-06-18 01:04:37 +0530

    ci/cd test
    Signed-off-by: Ansh Gujral <anshgujral1997@gmail.com>

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,7 +3,6 @@ var app = require("../server");
 
 describe("GET /", function () {
   it("respond with hello world", function (done) {
-    //navigate to root and check the response is "hello world"
     request(app).get("/").expect("hello world", done);
   });
 });
